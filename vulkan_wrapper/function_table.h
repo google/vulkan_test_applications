3c4a96c3a5f8de12517983f0cb958f8464439c58
ethan-conffx <ethan@conffx.com>
2019-12-17 15:14:43 -0800

    setup project for VK_EXT_hdr_metadata

--- vulkan_wrapper/function_table.h
+++ vulkan_wrapper/function_table.h
@@ -417,7 +417,8 @@ class DeviceFunctions {
         CONSTRUCT_LAZY_FUNCTION(vkCreateDescriptorUpdateTemplateKHR),
         CONSTRUCT_LAZY_FUNCTION(vkDestroyDescriptorUpdateTemplateKHR),
         CONSTRUCT_LAZY_FUNCTION(vkUpdateDescriptorSetWithTemplateKHR),
-        CONSTRUCT_LAZY_FUNCTION(vkResetQueryPoolEXT)
+        CONSTRUCT_LAZY_FUNCTION(vkResetQueryPoolEXT),
+        CONSTRUCT_LAZY_FUNCTION(vkSetHdrMetadataEXT)
 #if defined _WIN32
         ,
         CONSTRUCT_LAZY_FUNCTION(vkGetMemoryWin32HandleKHR),
@@ -542,6 +543,7 @@ class DeviceFunctions {
   LAZY_FUNCTION(vkDestroyDescriptorUpdateTemplateKHR);
   LAZY_FUNCTION(vkUpdateDescriptorSetWithTemplateKHR);
   LAZY_FUNCTION(vkResetQueryPoolEXT);
+  LAZY_FUNCTION(vkSetHdrMetadataEXT);
 #if defined _WIN32
   LAZY_FUNCTION(vkGetMemoryWin32HandleKHR);
   LAZY_FUNCTION(vkGetFenceWin32HandleKHR);
