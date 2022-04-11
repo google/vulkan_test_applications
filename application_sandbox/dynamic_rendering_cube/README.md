d30c31d547cff2b60a8b5dcc2b0d2a9afc90c4e2
Michael Anttila <michaelanttila@google.com>
2022-04-11 17:38:02 -0400

    Added test application for Dynamic Rendering
    This test application renders a cube on the screen using the dynamic
    rendering extension.  Two new functions (VkCmdBeginRenderingKHR and
    VkCmdEndRenderingKHR) were added to the function table in order to
    facilitate this.
    
    Tested on Windows with Nvidia.

---
+++ application_sandbox/dynamic_rendering_cube/README.md
@@ -0,0 +1,4 @@
+# Dynamic Rendering Cube
+
+This sample renders a rotating cube on the screen, but it does so
+using dynamic rendering.
---
+++ application_sandbox/dynamic_rendering_cube/main.cpp
@@ -0,0 +1,337 @@
+// Copyright 2022 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "application_sandbox/sample_application_framework/sample_application.h"
+#include "support/entry/entry.h"
+#include "vulkan_helpers/buffer_frame_data.h"
+#include "vulkan_helpers/helper_functions.h"
+#include "vulkan_helpers/vulkan_application.h"
+#include "vulkan_helpers/vulkan_model.h"
+
+#include <chrono>
+#include "mathfu/matrix.h"
+#include "mathfu/vector.h"
+
+using Mat44 = mathfu::Matrix<float, 4, 4>;
+using Vector4 = mathfu::Vector<float, 4>;
+
+namespace cube_model {
+#include "cube.obj.h"
+}
+const auto& cube_data = cube_model::model;
+
+uint32_t cube_vertex_shader[] =
+#include "cube.vert.spv"
+    ;
+
+uint32_t cube_fragment_shader[] =
+#include "cube.frag.spv"
+    ;
+
+struct CubeFrameData {
+  containers::unique_ptr<vulkan::VkCommandBuffer> command_buffer_;
+  containers::unique_ptr<vulkan::VkFramebuffer> framebuffer_;
+  containers::unique_ptr<vulkan::DescriptorSet> cube_descriptor_set_;
+};
+
+VkPhysicalDeviceDynamicRenderingFeaturesKHR kDynamicRenderingFeat{
+    VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DYNAMIC_RENDERING_FEATURES_KHR,
+    nullptr,
+    VK_TRUE
+};
+
+// This creates an application with 16MB of image memory, and defaults
+// for host, and device buffer sizes.
+class CubeSample : public sample_application::Sample<CubeFrameData> {
+ public:
+  CubeSample(const entry::EntryData* data)
+      : data_(data),
+        Sample<CubeFrameData>(
+            data->allocator(), data, 1, 512, 1, 1,
+            sample_application::SampleOptions()
+                .EnableMultisampling()
+                .AddDeviceExtensionStructure(&kDynamicRenderingFeat),
+            {0}, {VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME},
+            {VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME}),
+        cube_(data->allocator(), data->logger(), cube_data) {}
+
+  virtual void InitializeApplicationData(
+      vulkan::VkCommandBuffer* initialization_buffer,
+      size_t num_swapchain_images) override {
+    cube_.InitializeData(app(), initialization_buffer);
+
+    VkPhysicalDeviceDynamicRenderingFeaturesKHR ext_features{
+        VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DYNAMIC_RENDERING_FEATURES_KHR,
+        nullptr,
+        VK_FALSE,
+    };
+
+    VkPhysicalDeviceFeatures2 features = {
+        VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2, &ext_features};
+
+    app()->instance()->vkGetPhysicalDeviceFeatures2KHR(
+        app()->device().physical_device(), &features);
+
+    if (ext_features.dynamicRendering == VK_FALSE) {
+      data_->logger()->LogError("Dynamic rendering not supported");
+      exit(1);
+    }
+
+    cube_descriptor_set_layouts_[0] = {
+        0,                                  // binding
+        VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,  // descriptorType
+        1,                                  // descriptorCount
+        VK_SHADER_STAGE_VERTEX_BIT,         // stageFlags
+        nullptr                             // pImmutableSamplers
+    };
+    cube_descriptor_set_layouts_[1] = {
+        1,                                  // binding
+        VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,  // descriptorType
+        1,                                  // descriptorCount
+        VK_SHADER_STAGE_VERTEX_BIT,         // stageFlags
+        nullptr                             // pImmutableSamplers
+    };
+
+    pipeline_layout_ = containers::make_unique<vulkan::PipelineLayout>(
+        data_->allocator(),
+        app()->CreatePipelineLayout({{cube_descriptor_set_layouts_[0],
+                                      cube_descriptor_set_layouts_[1]}}));
+
+    vulkan::VkRenderPass render_pass(nullptr, nullptr, nullptr);
+    cube_pipeline_ = containers::make_unique<vulkan::VulkanGraphicsPipeline>(
+        data_->allocator(),
+        app()->CreateGraphicsPipeline(pipeline_layout_.get(),
+                                      &render_pass, 0));
+    VkFormat colorAttachmentFormat = render_format();
+    VkPipelineRenderingCreateInfoKHR pipeline_rendering_create_info = {
+        VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO_KHR,  // sType
+        nullptr,                                               // pNext
+        0,                                                     // viewMask
+        1,                          // colorAttachmentCount
+        &colorAttachmentFormat,     // pColorAttachmentFormats
+        VK_FORMAT_UNDEFINED,        // depthAttachmentFormat
+        VK_FORMAT_UNDEFINED         // stencilAttachmentFormat
+    };
+    cube_pipeline_->SetPipelineExtensions(&pipeline_rendering_create_info);
+    cube_pipeline_->AddShader(VK_SHADER_STAGE_VERTEX_BIT, "main",
+                              cube_vertex_shader);
+    cube_pipeline_->AddShader(VK_SHADER_STAGE_FRAGMENT_BIT, "main",
+                              cube_fragment_shader);
+    cube_pipeline_->SetTopology(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
+    cube_pipeline_->SetInputStreams(&cube_);
+    cube_pipeline_->SetViewport(viewport());
+    cube_pipeline_->SetScissor(scissor());
+    cube_pipeline_->SetSamples(num_samples());
+    cube_pipeline_->AddAttachment();
+    cube_pipeline_->Commit();
+
+    camera_data_ = containers::make_unique<vulkan::BufferFrameData<CameraData>>(
+        data_->allocator(), app(), num_swapchain_images,
+        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT);
+
+    model_data_ = containers::make_unique<vulkan::BufferFrameData<ModelData>>(
+        data_->allocator(), app(), num_swapchain_images,
+        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT);
+
+    float aspect =
+        (float)app()->swapchain().width() / (float)app()->swapchain().height();
+    camera_data_->data().projection_matrix =
+        Mat44::FromScaleVector(mathfu::Vector<float, 3>{1.0f, -1.0f, 1.0f}) *
+        Mat44::Perspective(1.5708f, aspect, 0.1f, 100.0f);
+
+    model_data_->data().transform = Mat44::FromTranslationVector(
+        mathfu::Vector<float, 3>{0.0f, 0.0f, -3.0f});
+  }
+
+  virtual void InitializeFrameData(
+      CubeFrameData* frame_data, vulkan::VkCommandBuffer* initialization_buffer,
+      size_t frame_index) override {
+    frame_data->command_buffer_ =
+        containers::make_unique<vulkan::VkCommandBuffer>(
+            data_->allocator(), app()->GetCommandBuffer());
+
+    frame_data->cube_descriptor_set_ =
+        containers::make_unique<vulkan::DescriptorSet>(
+            data_->allocator(),
+            app()->AllocateDescriptorSet({cube_descriptor_set_layouts_[0],
+                                          cube_descriptor_set_layouts_[1]}));
+
+    VkDescriptorBufferInfo buffer_infos[2] = {
+        {
+            camera_data_->get_buffer(),                       // buffer
+            camera_data_->get_offset_for_frame(frame_index),  // offset
+            camera_data_->size(),                             // range
+        },
+        {
+            model_data_->get_buffer(),                       // buffer
+            model_data_->get_offset_for_frame(frame_index),  // offset
+            model_data_->size(),                             // range
+        }};
+
+    VkWriteDescriptorSet write{
+        VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,  // sType
+        nullptr,                                 // pNext
+        *frame_data->cube_descriptor_set_,       // dstSet
+        0,                                       // dstbinding
+        0,                                       // dstArrayElement
+        2,                                       // descriptorCount
+        VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,       // descriptorType
+        nullptr,                                 // pImageInfo
+        buffer_infos,                            // pBufferInfo
+        nullptr,                                 // pTexelBufferView
+    };
+
+    app()->device()->vkUpdateDescriptorSets(app()->device(), 1, &write, 0,
+                                            nullptr);
+
+    (*frame_data->command_buffer_)
+        ->vkBeginCommandBuffer((*frame_data->command_buffer_),
+                               &sample_application::kBeginCommandBuffer);
+    vulkan::VkCommandBuffer& cmdBuffer = (*frame_data->command_buffer_);
+
+    VkImageMemoryBarrier image_memory_barrier = {
+        VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,    // sType
+        nullptr,                                   // pNext
+        VK_ACCESS_NONE_KHR,                        // srcAccessMask
+        VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,      // dstAccessMask
+        VK_IMAGE_LAYOUT_UNDEFINED,                 // oldLayout
+        VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,  // newLayout
+        0,                                         // srcQueueFamilyIndex
+        0,                                         // dstQueueFamilyIndex
+        swapchain_image(frame_data),               // image
+        {VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1}    // subresourceRange
+    };
+
+    cmdBuffer->vkCmdPipelineBarrier(
+        cmdBuffer, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
+        VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 0, 0, nullptr, 0,
+        nullptr, 1, &image_memory_barrier);
+
+    VkClearValue clear;
+    vulkan::MemoryClear(&clear);
+
+    VkRenderingAttachmentInfoKHR color_attachment = {
+        VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO_KHR,    // sType
+        nullptr,                                            // pNext
+        color_view(frame_data),                             // imageView
+        VK_IMAGE_LAYOUT_ATTACHMENT_OPTIMAL_KHR,             // imageLayout
+        VK_RESOLVE_MODE_NONE,                               // resolveMode
+        nullptr,                                            // resolveImageView
+        VK_IMAGE_LAYOUT_UNDEFINED,                          // resolveImageLayout;
+        VK_ATTACHMENT_LOAD_OP_CLEAR,                        // loadOp;
+        VK_ATTACHMENT_STORE_OP_STORE,                       // storeOp;
+        clear                                               // clearValue
+    };
+
+    VkRenderingInfoKHR rendering_info = {
+        VK_STRUCTURE_TYPE_RENDERING_INFO_KHR,   // sType
+        nullptr,                                // pNext
+        0,                                      // flags
+        {{0, 0},
+         {app()->swapchain().width(),
+          app()->swapchain().height()}},        // renderArea
+        1,                                      // layerCount
+        0,                                      // viewMask
+        1,                                      // colorAttachmentCount
+        &color_attachment,                      // pColorAttachments
+        nullptr,                                // pDepthAttachment
+        nullptr                                 // pStencilAttachment
+    };
+
+    cmdBuffer->vkCmdBeginRenderingKHR(cmdBuffer, &rendering_info);
+
+    cmdBuffer->vkCmdBindPipeline(cmdBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
+                                 *cube_pipeline_);
+    cmdBuffer->vkCmdBindDescriptorSets(
+        cmdBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
+        ::VkPipelineLayout(*pipeline_layout_), 0, 1,
+        &frame_data->cube_descriptor_set_->raw_set(), 0, nullptr);
+    cube_.Draw(&cmdBuffer);
+    cmdBuffer->vkCmdEndRenderingKHR(cmdBuffer);
+
+    image_memory_barrier.oldLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+    image_memory_barrier.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    cmdBuffer->vkCmdPipelineBarrier(
+        cmdBuffer, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+        VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT, 0, 0, nullptr, 0, nullptr, 1,
+        &image_memory_barrier);
+
+    (*frame_data->command_buffer_)
+        ->vkEndCommandBuffer(*frame_data->command_buffer_);
+  }
+
+  virtual void Update(float time_since_last_render) override {
+    model_data_->data().transform =
+        model_data_->data().transform *
+        Mat44::FromRotationMatrix(
+            Mat44::RotationX(3.14f * time_since_last_render) *
+            Mat44::RotationY(3.14f * time_since_last_render * 0.5f));
+  }
+  virtual void Render(vulkan::VkQueue* queue, size_t frame_index,
+                      CubeFrameData* frame_data) override {
+    // Update our uniform buffers.
+    camera_data_->UpdateBuffer(queue, frame_index);
+    model_data_->UpdateBuffer(queue, frame_index);
+
+    VkSubmitInfo init_submit_info{
+        VK_STRUCTURE_TYPE_SUBMIT_INFO,  // sType
+        nullptr,                        // pNext
+        0,                              // waitSemaphoreCount
+        nullptr,                        // pWaitSemaphores
+        nullptr,                        // pWaitDstStageMask,
+        1,                              // commandBufferCount
+        &(frame_data->command_buffer_->get_command_buffer()),
+        0,       // signalSemaphoreCount
+        nullptr  // pSignalSemaphores
+    };
+
+    app()->render_queue()->vkQueueSubmit(app()->render_queue(), 1,
+                                         &init_submit_info,
+                                         static_cast<VkFence>(VK_NULL_HANDLE));
+  }
+
+ private:
+  struct CameraData {
+    Mat44 projection_matrix;
+  };
+
+  struct ModelData {
+    Mat44 transform;
+  };
+
+  const entry::EntryData* data_;
+  containers::unique_ptr<vulkan::PipelineLayout> pipeline_layout_;
+  containers::unique_ptr<vulkan::VulkanGraphicsPipeline> cube_pipeline_;
+  containers::unique_ptr<vulkan::VkRenderPass> render_pass_;
+  VkDescriptorSetLayoutBinding cube_descriptor_set_layouts_[2];
+  vulkan::VulkanModel cube_;
+
+  containers::unique_ptr<vulkan::BufferFrameData<CameraData>> camera_data_;
+  containers::unique_ptr<vulkan::BufferFrameData<ModelData>> model_data_;
+};
+
+int main_entry(const entry::EntryData* data) {
+  data->logger()->LogInfo("Application Startup");
+  CubeSample sample(data);
+  sample.Initialize();
+
+  while (!sample.should_exit() && !data->WindowClosing()) {
+    sample.ProcessFrame();
+  }
+  sample.WaitIdle();
+
+  data->logger()->LogInfo("Application Shutdown");
+  return 0;
+}
